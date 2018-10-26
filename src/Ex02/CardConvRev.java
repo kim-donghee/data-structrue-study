4e6b1f63143ed67360db32fbe3b1e968e3a9f432
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-26 19:25:55 +0900

    cardconv

---
+++ src/Ex02/CardConvRev.java
@@ -0,0 +1,26 @@
+package Ex02;
+
+public class CardConvRev {
+	
+	static int cardConvR(int x, int r, char[] d) {
+		
+		int digits = 0;
+		String dchar = "0123456789ABCDEF";
+		
+		do {
+			d[digits++] = dchar.charAt(x % r);
+			x /= r;
+		} while (x!= 0);
+		
+		return digits;		
+	}
+	
+	public static void main(String[] args) {
+		char chArr[] = new char[32];
+		int i = cardConvR(30, 16, chArr) - 1;
+		for(; i>=0; i--) {
+			System.out.print(chArr[i]);
+		}
+	}
+
+}
