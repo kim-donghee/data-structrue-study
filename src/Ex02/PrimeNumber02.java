8538c4b0646aa0dfedd60f4d5041730c2023ae4a
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-27 19:30:10 +0900

    primenumber

--- src/Ex02/PrimeNumber02.java
+++ src/Ex02/PrimeNumber02.java
@@ -1,22 +1,28 @@
 package Ex02;
 
-public class PrimeNumber01 {
-
+public class PrimeNumber02 {
+	
 	public static void main(String[] args) {
 		int ptr = 0;
 		int counter = 0;
 		int[] prime = new int[512];
 		
-		for(int n = 2; n <= 1000; n++) {
-			int i;
-			
-			for(i = 2; i < n; i++) {
-				counter++;
-				if(n % i == 0) break;
+		prime[ptr++] = 2;
+		prime[ptr++] = 3;
+		
+		for(int n = 5; n <= 1000; n+=2) {
+			boolean flag = false;
+			for(int i=1; prime[i] * prime[i] <= n; i++) {
+				counter += 2;
+				if(n % prime[i] == 0) {
+					flag = true;
+					break;
+				}
 			}
-			if(n == i) {
+			if(!flag) {
 				prime[ptr++] = n;
-			}			
+				counter++;
+			}
 		}
 		
 		for(int i = 0; i < ptr; i++) {
@@ -27,5 +33,5 @@ public class PrimeNumber01 {
 		System.out.println("연산 횟수 : " + counter);
 		
 	}
-
+	
 }
