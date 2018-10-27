6d16a20cd692f74c9cddb7c594cc214692839e46
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-27 18:24:54 +0900

    ...

--- src/Ex02/PrimeNumber01.java
+++ src/Ex02/PrimeNumber01.java
@@ -2,7 +2,10 @@ package Ex02;
 
 public class PrimeNumber01 {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		int ptr = 0;
+		int[] prime = new int[512];
+		
 		for(int n = 2; n <= 1000; n++) {
 			int i;
 			
@@ -10,8 +13,12 @@ public class PrimeNumber01 {
 				if(n % i == 0) break;
 			}
 			if(n == i) {
-				System.out.println(n);
+				prime[ptr++] = n;
 			}			
+		}
+		
+		for(int i = 0; i < ptr; i++) {
+			System.out.println(prime[i]);
 		}
 		
 	}
