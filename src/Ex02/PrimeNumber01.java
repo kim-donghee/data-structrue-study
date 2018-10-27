b0b062106309574f8164197a81be576cfa10dd8e
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-27 18:31:47 +0900

    소수구하기

--- src/Ex02/PrimeNumber01.java
+++ src/Ex02/PrimeNumber01.java
@@ -4,12 +4,14 @@ public class PrimeNumber01 {
 
 	public static void main(String[] args) {
 		int ptr = 0;
+		int counter = 0;
 		int[] prime = new int[512];
 		
 		for(int n = 2; n <= 1000; n++) {
 			int i;
 			
 			for(i = 2; i < n; i++) {
+				counter++;
 				if(n % i == 0) break;
 			}
 			if(n == i) {
@@ -20,6 +22,8 @@ public class PrimeNumber01 {
 		for(int i = 0; i < ptr; i++) {
 			System.out.println(prime[i]);
 		}
+		
+		System.out.println("연산 횟수 : " + counter);
 		
 	}
 
