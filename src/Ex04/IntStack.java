a69f515a85529aefd91a64223791417da871a7d8
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-28 14:59:44 +0900

    stack

--- src/Ex04/IntStack.java
+++ src/Ex04/IntStack.java
@@ -2,9 +2,9 @@ package Ex04;
 
 public class IntStack {
 	
-	private int max;
-	private int ptr;
-	private int stk[];
+	private int max;	// 스택 용량
+	private int ptr;	// 스택 포인터
+	private int stk[];	// 스택
 	
 	public class EmptyIntStackException extends RuntimeException { }
 	
@@ -48,6 +48,23 @@ public class IntStack {
 		return stk[ptr-1];
 	}
 	
+	public int indexOf(int x) {
+		if(isEmpty()) {
+			System.out.println("스택이 비어있습니다.");
+		} else {
+			for(int i = ptr-1; i>=0; i--) {
+				if(stk[i] == x)
+					return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public void clear() {
+		ptr = 0;
+	}
+	
 	public int capacity() {
 		return max;
 	}
@@ -69,24 +86,11 @@ public class IntStack {
 			System.out.println("스택이 비어있습니다.");
 		} else {
 			System.out.print("stack values : [");
-			for(int i = ptr-1; i >= 0; i--) {
+			for(int i=0; i<ptr; i++) {
 				System.out.print(stk[i] + ", ");
 			}
 			System.out.println("]");
 		}
-	}
-	
-	public int indexOf(int x) {
-		if(isEmpty()) {
-			System.out.println("스택이 비어있습니다.");
-		} else {
-			for(int i = ptr-1; i>=0; i--) {
-				if(stk[i] == x)
-					return i;
-			}
-		}
-		
-		return -1;
 	}
 	
 	
