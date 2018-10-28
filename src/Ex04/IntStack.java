b235b09a891951c67f524967affb2c8e9c9a9c44
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-28 15:38:22 +0900

    stack

--- src/Ex04/IntStack.java
+++ src/Ex04/IntStack.java
@@ -49,15 +49,10 @@ public class IntStack {
 	}
 	
 	public int indexOf(int x) {
-		if(isEmpty()) {
-			System.out.println("스택이 비어있습니다.");
-		} else {
-			for(int i = ptr-1; i>=0; i--) {
-				if(stk[i] == x)
-					return i;
-			}
+		for(int i = ptr-1; i>=0; i--) {
+			if(stk[i] == x)
+				return i;
 		}
-		
 		return -1;
 	}
 	
@@ -81,16 +76,12 @@ public class IntStack {
 		return ptr>=max;
 	}
 	
-	public void dump() {
-		if(isEmpty()) {
-			System.out.println("스택이 비어있습니다.");
-		} else {
-			System.out.print("stack values : [");
-			for(int i=0; i<ptr; i++) {
-				System.out.print(stk[i] + ", ");
-			}
-			System.out.println("]");
+	public void dump() {		
+		System.out.print("stack values : [");
+		for(int i=0; i<ptr; i++) {
+			System.out.print(stk[i] + ", ");
 		}
+		System.out.println("]");
 	}
 	
 	
