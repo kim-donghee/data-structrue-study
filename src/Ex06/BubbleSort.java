9422ec759d7f5996e73fb62678bfc816c8067a66
Kim01 <Kim01@DESKTOP-SG1SCT1>
2018-10-29 15:37:53 +0900

    bubble sort

--- src/Ex06/BubbleSort.java
+++ src/Ex06/BubbleSort.java
@@ -4,12 +4,14 @@ public class BubbleSort {
 	
 	static void bubbleSort(int[] a) {
 		
+		int counter = 0;
 		int n = a.length;
 		boolean exchg;
 		
 		for(int i=0; i<n; i++) {
 			exchg = false;
 			for(int j=n-1; j>i; j--) {
+				counter++;
 				if(a[j-1] > a[j]) {
 					int temp = a[j-1];
 					a[j-1] = a[j];
@@ -20,11 +22,14 @@ public class BubbleSort {
 			if(!exchg) break;
 		}
 		
+		System.out.println(counter);
+		
 	}
 
 	public static void main(String[] args) {
 		
 		int n = 10;
+//		int arr[] = { 8, 4, 10, 9, 5, 7, 6, 2, 2, 10 };
 		int arr[] = new int[n]; 
 		
 		for(int i=0; i<n; i++) {
